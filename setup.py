2a93b3188633cefc4e7c8bd2c10aee22edaf0e0b
DavidHoenisch <dh1689@pm.me>
2025-06-29 21:13:51 -0700

    cleaned up comments in the setup.py file that are not needed

--- setup.py
+++ setup.py
@@ -1,38 +1,22 @@
 import setuptools
 
-# Read the contents of your README file
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-# Read the requirements from the requirements.txt file
 with open("requirements.txt", "r", encoding="utf-8") as f:
     install_requires = f.read().splitlines()
 
 setuptools.setup(
-    # Name of the package
     name="scythe",
-
-    # Version of the package
     version="0.1.0",
-
-    # Author information
     author="EpykLab",
     author_email="cyber@epyklab.com",
-
-    # A short, one-sentence summary of the package
     description="An extensible framework for emulating attacker TTPs with Selenium.",
-
-    # A long description of your package, read from the README file
     long_description=long_description,
     long_description_content_type="text/markdown",
-
-    # The URL for the project's homepage
     url="https://github.com/EpykLab/scythe",
+    license="MIT",
 
-    # The license for the package
-    license="MIT", # Or another license like "Apache-2.0"
-
-    # Classifiers help users find your project
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -41,17 +25,13 @@ setuptools.setup(
         "Intended Audience :: Developers",
         "Intended Audience :: Information Technology",
         "Topic :: Security",
-        "Framework :: Pytest", # If you plan to use pytest for testing
+        "Framework :: Pytest",
     ],
 
-    # Specify the Python version requirements
     python_requires=">=3.8",
 
-    # Automatically find all packages in the project
-    # This will find `ttp_framework`, `ttp_framework.core`, etc.
     packages=setuptools.find_packages(exclude=["tests*", "examples*"]),
 
-    # List of dependencies required by the package
     install_requires=install_requires,
 
     # Entry points can be used to create command-line scripts
