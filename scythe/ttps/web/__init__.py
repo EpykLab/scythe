3d626b8d344318df91874c469948393f4d4a202c
DavidHoenisch <dh1689@pm.me>
2025-11-04 13:13:28 -0800

    fix for syntax error

--- scythe/ttps/web/__init__.py
+++ scythe/ttps/web/__init__.py
@@ -1,12 +1,12 @@
 from .login_bruteforce import LoginBruteforceTTP
 from .sql_injection import InputFieldInjector, URLManipulation
-from .uuid_guessing import UUIDGuessingTTP
+from .uuid_guessing import GuessUUIDInURL
 from .request_flooding import RequestFloodingTTP
 
 __all__ = [
     'LoginBruteforceTTP',
     'InputFieldInjector', 
     'URLManipulation',
-    'UUIDGuessingTTP',
+    'GuessUUIDInURL',
     'RequestFloodingTTP'
 ]
